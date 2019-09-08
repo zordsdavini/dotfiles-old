1d97be00abdbf01599bd4f52ef3538f923a4aca9
Arnas Udovicius <arnas.u@estina.lt>
2019-09-08 22:12:39 +0300

    Autocommit: Sun 08 Sep 2019 10:12:39 PM EEST

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Zordsdavini dotfiles
 
-Place to save configs and use with fork of LARBS [https://github.com/LukeSmithxyz/LARBS] to restore my environment
+Place to save configs and use with fork of [LARBS](https://github.com/LukeSmithxyz/LARBS) to restore my environment
---
+++ collect.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo Starting collecting configs...
+
+PWD=`pwd`
+while read TARGET; do
+    SOURCE=`echo $TARGET | sed "s,~,$HOME,g"`
+    DESTINATION=`echo $TARGET | sed "s,\.,\$,g" | sed "s,~,$PWD,g" | sed "s,\/$,,g"`
+    DESTINATION=`dirname $DESTINATION`
+    mkdir -p $DESTINATION
+    echo $TARGET
+    cp $SOURCE $DESTINATION
+done < ./list
+
+echo Commiting...
+git add .
+git commit -a -m "Autocommit: `date`"
+git push
+
+echo Done.
